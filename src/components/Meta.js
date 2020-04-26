08fd4e2f5f6d539057d2ead32daccafa9df31031
vegardloewe <vegardloewe96@gmail.com>
2020-04-26 07:57:06 +0200

    commit

--- src/components/Meta.js
+++ src/components/Meta.js
@@ -54,9 +54,9 @@ export default class Meta extends Component {
 
         <meta property="og:locale" content="en_US" />
         <meta property="og:site_name" content={siteTitle} />
-        <meta name="twitter:description" content={siteDescription} />
-        <meta name="twitter:title" content={siteTitle} />
-        <meta name="twitter:image" content={absoluteImageUrl} />
+        <meta property="twitter:description" content={siteDescription} />
+        <meta property="twitter:title" content={siteTitle} />
+        <meta property="twitter:image" content={absoluteImageUrl} />
         <meta property="og:image:secure_url" content={absoluteImageUrl} />
         <meta property="og:image" content={absoluteImageUrl} />
         <meta property="twitter:card" content={absoluteImageUrl} />
