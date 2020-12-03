4a072be8b78d3107d5010b698829496faf76ee4b
Vegard <vegard@lowedesign.no>
2020-12-03 10:37:41 +0100

    commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -140,12 +140,6 @@ module.exports = {
         enableIdentityWidget: true
       }
     },
-    {
-      resolve: 'gatsby-plugin-mailchimp',
-      options: {
-      	endpoint: '',
-      },
-    },
     'gatsby-plugin-netlify' // make sure to keep it last in the array
   ]
 }
